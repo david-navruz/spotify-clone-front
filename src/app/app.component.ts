1656e6d53069a938eba95b6f302bcf1f426fafbc
David <david.navruz55@gmail.com>
2025-02-11 18:40:05 +0100

    Adding login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome
 import {fontAwesomeIcons} from './shared/font-awesome-icons';
 import {faCirclePlay} from '@fortawesome/free-solid-svg-icons';
 import {NavigationComponent} from './layout/navigation/navigation.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {LibraryComponent} from './layout/library/library.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule, NavigationComponent],
+  imports: [RouterOutlet, FontAwesomeModule, NavigationComponent, HeaderComponent, LibraryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/layout/library/library.component.ts
+++ src/app/layout/library/library.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-library',
   standalone: true,
   imports: [
-    FaIconComponent
+    FaIconComponent,
+    RouterModule
   ],
   templateUrl: './library.component.html',
   styleUrl: './library.component.scss'
 })
 export class LibraryComponent {
+
 
   isLoading:boolean = false;
 
